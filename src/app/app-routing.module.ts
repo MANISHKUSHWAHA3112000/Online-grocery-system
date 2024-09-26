f9be4c8a255707b86094d5e2ef9731ac7eda707e
Ishaa1312 <isha.b.agrawal@capgemini.com>
2024-09-26 13:11:21 +0530

    Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [  
   { path: '', component: LandingPageComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
